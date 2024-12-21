1832e9733a353e6bcfaed0486dba081e0dc21455
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2024-12-21 19:34:38 -0300

    remote-state: adding repository

---
+++ main.tf
@@ -0,0 +1,19 @@
+# Configures the remote backend for Terraform
+terraform {
+  backend "s3" {
+    bucket         = "remote-state-edsoncarlos"          # Name of the S3 bucket
+    key            = "state/terraform.tfstate"           # Path to store the Terraform state file
+    region         = "us-east-1"                         # AWS region
+    dynamodb_table = "terraform-locks"                   # Name of the DynamoDB table for state locking
+    encrypt        = true                                # Enables encryption for the state file
+  }
+}
+
+resource "aws_instance" "remote-state-terraform" {
+  ami           = "ami-0c02fb55956c7d316" 
+  instance_type = "t2.micro"
+
+  tags = {
+    Name = "Example Instance-Edson-Carlos"
+  }
+}
