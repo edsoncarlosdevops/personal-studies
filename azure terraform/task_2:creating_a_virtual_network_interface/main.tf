4d0b1b96dd641722d7bcd1e4b18f713041352fba
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2025-06-03 16:10:32 -0300

    add task 02

---
+++ azure terraform/task_2:creating_a_virtual_network_interface/main.tf
@@ -0,0 +1,37 @@
+resource "azurerm_resource_group" "appgrp" {
+  name     = "app-grp"
+  location = local.resource_location
+}
+
+resource "azurerm_virtual_network" "app_network" {
+  name                = local.virtual_network.name
+  address_space       = local.virtual_network.address_prefixes
+  location            = azurerm_resource_group.example.location
+  resource_group_name = azurerm_resource_group.example.name
+}
+
+resource "azurerm_subnet" "websubnet01" {
+  name                 = local.subnets[0].name
+  resource_group_name  = azurerm_resource_group.appgrp.name
+  virtual_network_name = azurerm_virtual_network.app_network.name
+  address_prefixes     = local.subnets[0].address_prefixes
+}
+
+resource "azurerm_subnet" "appsubnet01" {
+  name                 = local.subnets[1].name
+  resource_group_name  = azurerm_resource_group.appgrp.name
+  virtual_network_name = azurerm_virtual_network.app_network.name
+  address_prefixes     = local.subnets[1].address_prefixes
+}
+
+resource "azurerm_network_interface" "example" {
+  name                = "webinterface01"
+  location            = local.resource_location
+  resource_group_name = azurerm_resource_group.appgrp.name
+
+  ip_configuration {
+    name                          = "internal"
+    subnet_id                     = azurerm_subnet.websubnet01.id
+    private_ip_address_allocation = "Dynamic"
+  }
+}
