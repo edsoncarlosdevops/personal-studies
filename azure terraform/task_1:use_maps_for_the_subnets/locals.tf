3bf06b9119565392c1b41536e752c75ee22d15d0
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2025-06-03 16:12:40 -0300

    identando o locals

--- azure terraform/task_1:use_maps_for_the_subnets/locals.tf
+++ azure terraform/task_1:use_maps_for_the_subnets/locals.tf
@@ -1,8 +1,8 @@
 locals {
   resource_location = "West Europe"
   virtual_network = {
-    name                = "appp-network"
+    name             = "appp-network"
     address_prefixes = ["10.0.0.0/16"]
   }
-  subnet_address_prefix=["10.0.1.0/24", "10.0.2.0/24"]
+  subnet_address_prefix = ["10.0.1.0/24", "10.0.2.0/24"]
 }
