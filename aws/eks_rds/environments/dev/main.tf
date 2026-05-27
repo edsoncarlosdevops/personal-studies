ba5aa0624b934ba56b86199b897164796741e1b0
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2026-05-27 20:23:11 -0300

    corrige master username de admin para dbadmin (palavra reservada)

--- aws/eks_rds/environments/dev/main.tf
+++ aws/eks_rds/environments/dev/main.tf
@@ -36,7 +36,7 @@ module "rds" {
   vpc_id             = module.vpc.vpc_id
   private_subnet_ids = module.vpc.private_subnet_ids
   db_name            = "appdb"
-  db_username        = "admin"
+  db_username        = "dbadmin"
   db_password        = random_password.db_password.result
   allowed_cidr       = "10.0.0.0/16"
 }
