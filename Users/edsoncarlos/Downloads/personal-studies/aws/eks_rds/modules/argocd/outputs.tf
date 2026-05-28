6417f62484acd072e8ccac73bab2ba3df25afaa4
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2026-05-28 09:05:30 -0300

    remove pasta duplicada Users/...

--- Users/edsoncarlos/Downloads/personal-studies/aws/eks_rds/modules/argocd/outputs.tf
+++
@@ -1,8 +0,0 @@
-output "argocd_namespace" {
-  value = kubernetes_namespace.argocd.metadata[0].name
-}
-
-output "argocd_admin_password" {
-  value     = data.kubernetes_secret.argocd_admin.data["password"]
-  sensitive = true
-}
