f08fd1cf0131cbb77984b042fc2d32635d0d59e3
edsoncarlosdevops <edsoncarlos.ec40@gmail.com>
2026-06-12 17:32:00 -0300

    chore: remove modulo app-rds (nao utilizado) + docs de conexao RDS
    - Remove aws/eks_rds/modules/app-rds/ (nunca foi ativado no main.tf)
    - Remove aws/eks_rds/deploy/apps/app-rds/ (terragrunt orfao)
    - Adiciona TODO no api-pedidos.yaml com ideias para conectar no RDS
    - Adiciona secao no setup-lab.sh com passo a passo da integracao RDS

--- aws/eks_rds/modules/app-rds/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "app_service_name" {
-  description = "Kubernetes service name for the app"
-  value       = kubernetes_service.app.metadata[0].name
-}
-
-output "app_namespace" {
-  description = "Namespace where the app is deployed"
-  value       = kubernetes_namespace.app.metadata[0].name
-}
